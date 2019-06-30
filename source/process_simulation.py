39b1e9da17f46fcc89ee5200e9ba60b3e257dac4
Bob Myhill <myhill.bob@gmail.com>
2019-06-30 12:49:03 +0100

    finished coordination plotting

--- source/process_simulation.py
+++ source/process_simulation.py
@@ -17,7 +17,7 @@ class Configuration(object):
         self.element_list = [item for sublist in [[e]*n_atoms[i]
                                                   for i, e in enumerate(elements)]
                              for item in sublist]
-        
+
     def process(self, cutoff_scaling):
         print('Processing {0}'.format(self.name))
         d = self.unscaled_data        
@@ -35,9 +35,9 @@ class Configuration(object):
                               d + [-1., -1., 1.], d + [1., 1., -1.],
                               d + [-1., 1., 1.], d + [1., -1., -1.],
                               d + [-1., 1., -1.], d + [1., -1., 1.],))*self.box_dimensions
-        
+
         self.triangulation = Delaunay(self.xyz)
-        
+
         self.connectivity = [set([]) for i in self.xyz]
         for simplex in self.triangulation.simplices:
             for p in simplex:
@@ -46,7 +46,7 @@ class Configuration(object):
         # remove self-connections and disallowed connections
         for p in range(len(self.xyz)):
             self.connectivity[p].remove(p)
-    
+
             # remove disallowed connections
             removed_lengths = []
             for p1 in list(self.connectivity[p]):
@@ -57,7 +57,7 @@ class Configuration(object):
                 if self.element_list[p%self.total_atoms] != 'O' and self.element_list[p1%self.total_atoms] != 'O': # remove Mg-Mg, Mg-Si, Si-Si
                     self.connectivity[p].remove(p1)
                     removed_lengths.append(np.linalg.norm(self.xyz[p] - self.xyz[p1]))
-                
+
             # remove allowed connections that are longer than the shortest disallowed connection
             # IDEA: if necessary (for very anisotropic sites?),
             #       could remove points outside the polyhedron
@@ -117,11 +117,11 @@ class Simulation(object):
             print(self.n_atoms)
             print(self.total_atoms)
 
-            
+
             self.element_list = [item for sublist in [[e]*self.n_atoms[i]
                                                       for i, e in enumerate(self.elements)]
                                  for item in sublist]
-            
+
             self.n_configurations = (len(datalines) - 7)/(self.total_atoms+1)
             print('Number of configurations: {0}'.format(self.n_configurations))
             self.configurations = []
@@ -147,12 +147,24 @@ sim = Simulation(filename=filename)
 
 
 coordination = {'Mg': [], 'Si': [], 'O': []}
+O_coordination = {'Mg': [], 'Si': []}
+
 next_nearest_neighbours = []
 elements = []
 for i_conf in range(14000, 15000):
     sim.configurations[i_conf].process(cutoff_scaling={'Mg': 1., 'Si': 1., 'O': 1.})
     for i in range(0,96):
         coordination[sim.element_list[i%sim.total_atoms]].append(len(sim.configurations[i_conf].connectivity[i]))
+
+        if sim.element_list[i%sim.total_atoms] == 'O':
+            O_coordination['Mg'].append(0)
+            O_coordination['Si'].append(0)
+
+            for p_connector in sim.configurations[i_conf].connectivity[i]:
+                if sim.element_list[p_connector%sim.total_atoms] == 'Mg':
+                    O_coordination['Mg'][-1] += 1
+                if sim.element_list[p_connector%sim.total_atoms] == 'Si':
+                    O_coordination['Si'][-1] += 1
 
         next_nearest_atoms = sim.configurations[i_conf].next_nearest[i]
         elements.append(sim.element_list[i%sim.total_atoms])
@@ -161,38 +173,107 @@ for i_conf in range(14000, 15000):
             next_nearest_neighbours[-1][sim.element_list[n%sim.total_atoms]] += 1.
 
 
-Mg_Mg = [[] for i in range(40)]
-Si_Mg = [[] for i in range(40)]
+c_max = 20
+n_next_nearest = {'Mg': {'Mg': [0 for i in range(c_max)],
+                       'Si': [0 for i in range(c_max)],
+                       'cation': [0 for i in range(c_max)]},
+                'Si': {'Mg': [0 for i in range(c_max)],
+                       'Si': [0 for i in range(c_max)],
+                       'cation': [0 for i in range(c_max)]}}
+
+f_next_nearest = {'Mg': {'Mg': [[] for i in range(c_max)],
+                       'Si': [[] for i in range(c_max)]},
+                'Si': {'Mg': [[] for i in range(c_max)],
+                       'Si': [[] for i in range(c_max)]}}
 for i in range(len(elements)):
     n = next_nearest_neighbours[i]
-    if elements[i] == 'Mg':
-        Mg_Mg[int(n['Mg'] + n['Si'] + n['O'])].append(n['Mg']/(n['Mg'] + n['Si'] + n['O']))
-    if elements[i] == 'Si':
-        Si_Mg[int(n['Mg'] + n['Si'] + n['O'])].append(n['Mg']/(n['Mg'] + n['Si'] + n['O']))
+    if elements[i] == 'Mg' or elements[i] == 'Si':
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
-plt.plot(range(40), [np.average(map(float, fs)) for fs in Mg_Mg], label='Mg-Mg')
-plt.plot(range(40), [np.average(map(float, fs)) for fs in Si_Mg], label='Si-Mg')
+        if int(n['Mg']) < c_max:
+            n_next_nearest[elements[i]]['Mg'][int(n['Mg'])] += 1
 
-ax.set_xlim(0,)
-ax.set_ylim(0,1)
+        if int(n['Si']) < c_max:
+            n_next_nearest[elements[i]]['Si'][int(n['Si'])] += 1
 
-ax.set_xlabel('Coordination number A+B around atom A')
-ax.set_ylabel('Fraction atom B around atom A')
+        if int(n['Mg'] + n['Si']) < c_max:
+            n_next_nearest[elements[i]]['cation'][int(n['Mg'] + n['Si'])] += 1
+
+        if int(n['Mg'] + n['Si'] + n['O']) < c_max:
+            f_next_nearest[elements[i]]['Mg'][int(n['Mg'] + n['Si'] + n['O'])].append(n['Mg']/(n['Mg'] + n['Si'] + n['O']))
+            f_next_nearest[elements[i]]['Si'][int(n['Mg'] + n['Si'] + n['O'])].append(n['Si']/(n['Mg'] + n['Si'] + n['O']))
+
+
+
+fig = plt.figure(figsize=(15,10))
+ax = [fig.add_subplot(2,2,i) for i in range(1, 5)]
+
+
+#
+for a1 in ['Mg', 'Si']:
+    for a2 in ['Mg', 'Si']:
+        ax[0].plot(range(c_max),
+                   np.array(map(float, n_next_nearest[a1][a2]))/sum(n_next_nearest[a1][a2]),
+                   label='{0}-{1}'.format(a1, a2))
+    for a2 in ['cation']:
+        ax[1].plot(range(c_max),
+                   np.array(map(float, n_next_nearest[a1][a2]))/sum(n_next_nearest[a1][a2]),
+                   label='{0}-{1}'.format(a1, a2))
+
+
+ax[0].set_xlim(0,c_max)
+ax[0].set_xticks(range(0, c_max, 2))
+ax[0].set_xlabel('coordination')
+ax[0].set_ylabel('atom fraction')
+ax[0].legend()
+
+
+ax[1].set_xlim(0,c_max)
+ax[1].set_xticks(range(0, c_max, 2))
+ax[1].set_xlabel('coordination')
+ax[1].set_ylabel('atom fraction')
+ax[1].legend()
+
+# Nearest neighbour coordination
+coordinations = range(c_max+1)
+for atom in ['Mg', 'Si', 'O']:
+    xs, cs = np.unique(coordination[atom], return_counts=True)
+    counts = np.zeros_like(coordinations).astype(float)
+    counts[xs] = map(float, cs)
+    if atom == 'O':
+        ax[2].plot(coordinations, counts/sum(counts), label='{0}-cation'.format(atom))
+    else:
+        ax[2].plot(coordinations, counts/sum(counts), label='{0}-O'.format(atom))
+
+for atom in ['Mg', 'Si']:
+    xs, cs = np.unique(O_coordination[atom], return_counts=True)
+    counts = np.zeros_like(coordinations).astype(float)
+    counts[xs] = map(float, cs)
+    ax[2].plot(coordinations, counts/sum(counts), label='O-{0}'.format(atom))
+
+ax[2].set_xlim(0,c_max/2.)
+ax[2].set_xticks(range(0, c_max/2, 2))
+ax[2].set_xlabel('coordination')
+ax[2].set_ylabel('atom fraction')
+ax[2].legend()
+
+
+# Coordination fractions
+ax[3].plot(range(c_max), [np.average(map(float, fs)) for fs in f_next_nearest['Mg']['Mg']], label='Mg-Mg')
+ax[3].plot(range(c_max), [np.average(map(float, fs)) for fs in f_next_nearest['Si']['Mg']], label='Si-Mg')
+
+
+ax[3].set_xlim(0,c_max)
+ax[3].set_xticks(range(0, c_max, 2))
+ax[3].set_ylim(0,1)
+
+ax[3].set_xlabel('Coordination number A+B around atom A')
+ax[3].set_ylabel('Fraction atom B around atom A')
+
+ax[3].legend()
+
 
 base = os.path.basename(filename)
 basename = os.path.splitext(base)[0]
-plt.legend()
-
-fig.savefig(basename+'_coordination_fractions.pdf')
-plt.show()
-
-
-
-bins = np.linspace(0.5, 12.5, 13)
-for atom in ['Mg', 'Si', 'O']:
-    plt.hist(coordination[atom], bins = bins, histtype='step', label=atom)
-plt.legend()
+fig.savefig('output/'+basename+'_coordination_fractions.pdf')
 plt.show()
 
